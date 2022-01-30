b2abf2657b96c37e9246751d542777d373c476aa
sfoos1121 <sandiefoos@gmail.com>
2022-01-29 19:52:56 -0500

    Adding front end

---
+++ client/src/Views/History.js
@@ -0,0 +1,14 @@
+import MoodTrackerLog from "../Components/MoodTrackerLog";
+import Nav from "../Components/Nav";
+
+const History = () => {
+
+    return (
+        <div>
+            <Nav />
+            <MoodTrackerLog />
+        </div>
+    )
+}
+
+export default History;
---
+++ client/src/Views/Resources.js
@@ -0,0 +1,14 @@
+import ResourceList from "../Components/ResourceList";
+import Nav from "../Components/Nav";
+
+const Resources = () => {
+
+    return (
+        <div>
+            <Nav />
+            <ResourceList />
+        </div>
+    )
+}
+
+export default Resources;
